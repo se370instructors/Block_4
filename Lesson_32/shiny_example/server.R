59f52f2b7b41ca59da3fa61ddf1fbac9c5e71126
David Beskow <dnbeskow@gmail.com>
2023-04-20 07:32:30 -0400

    added soln to lsn34

--- Lesson_32/shiny_example/server.R
+++
@@ -1,7 +0,0 @@
-library(ggplot2) 
-shinyServer(function(input, output) {
-  output$myplot <- renderPlot({
-    ggplot(data = mtcars, aes_string(x = input$xAxis, y = input$yAxis)) +
-      geom_point(size = input$pointSize) + theme_minimal()
-  })
-})
